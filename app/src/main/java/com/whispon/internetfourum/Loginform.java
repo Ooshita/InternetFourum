eb9ae17ea618374272e55c4cbbbf65e053cf1ef2
Ooshita <ponkichi0213@gmail.com>
2015-03-03 03:15:25 +0900

    アカウント登録画面を作成中だけど上手くいかない。基礎的な知識が無いので引数や値の渡し、参照を勉強する

--- app/src/main/java/com/whispon/internetfourum/Loginform.java
+++ app/src/main/java/com/whispon/internetfourum/Loginform.java
@@ -1,8 +1,10 @@
 package com.whispon.internetfourum;
 
 import android.app.Application;
+import android.content.Intent;
 import android.util.Log;
 
+import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseInstallation;
 import com.parse.ParseUser;
@@ -17,22 +19,26 @@ public class Loginform extends Application{
 
     public static final String TAG = Loginform.class.getSimpleName();
 
-    @Override
-    public void onCreate() {
+    //@Override
+    public String onCreate(String a,String b,String c,String d) {
         super.onCreate();
 
         // Add your initialization code here
         //Parse.initialize(this, "CJ16cXd7zEvOCf61V2BS9BoGePQpA4IXcqViNC0w", "fJicEIyMy4ErOetLX7DgubsZRo5xlWJ1VtwhpvIl");        // Save the current Installation to Parse.
 
         ParseUser user = new ParseUser();
-        user.setUsername("my name");
-        user.setPassword("my pass");
-        user.setEmail("email@example.com");
+        user.setUsername(a);
+        user.setPassword(b);
+        user.setEmail(c);
+
 
         // other fields can be set just like with ParseObject
-        user.put("phone", "650-555-0000");
+        user.put("phone", d);
 
+
+        //ユーザー登録が済んでいたらえらーになる
         user.signUpInBackground(new SignUpCallback() {
+
             public void done(com.parse.ParseException e) {
                 if (e == null) {
                     Log.d(TAG, "サインアップ成功");
@@ -41,5 +47,26 @@ public class Loginform extends Application{
                 }
             }
         });
+
+
+        ParseUser.logInInBackground(a, b, new LogInCallback() {
+            public void done(ParseUser user, com.parse.ParseException e) {
+                if (user != null) {
+                    Log.d(TAG, "ログイン成功");
+                } else {
+                    Log.e(TAG, "エラーによりログイン失敗");
+                    Intent intentAddUser = new Intent();
+                    intentAddUser.setClassName("com.whispon.internetfourum","com.whispon.internetfourum.AddUser");
+                    startActivity(intentAddUser);
+                }
+            }
+        });
+
+        Intent intentAddUser = new Intent();
+        intentAddUser.setClassName("com.whispon.internetfourum","com.whispon.internetfourum.AddUser");
+        startActivity(intentAddUser);
+
+        return a;
+
     }
 }
